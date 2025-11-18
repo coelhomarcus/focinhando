f77b640d47d01cf677f92c918d70a391cee115ff
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-11-18 00:06:23 -0300

    fix router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-   <BrowserRouter>
+   <HashRouter>
       <App />
-   </BrowserRouter>,
+   </HashRouter>,
 )
